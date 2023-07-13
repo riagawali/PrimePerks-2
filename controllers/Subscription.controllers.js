fe991dfab5a54c117c7915a047dcd013cc779063
riagawali <101542435+riagawali@users.noreply.github.com>
2023-07-13 17:37:53 +0530

    Subscription.controllers.js

--- controllers/Subscription.controllers.js
+++ controllers/Subscription.controllers.js
@@ -1,21 +1,3 @@
-// const Subscription = require("../model/Subscription");
-
-// const createSubscription = async (req, res) => {
-//   try {
-//     const { userId, planId } = req.body;
-//     const subscription = new Subscription({ userId, planId });
-//     await subscription.save();
-//     res.json({ message: "Subscription created successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to create subscription" });
-//   }
-// };
-
-// module.exports = {
-//   createSubscription,
-// };
-
 const subscriptionService = require("../services/subscriptionService");
 
 const createSubscription = async (req, res) => {
