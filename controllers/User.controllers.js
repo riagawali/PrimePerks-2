eb728ed5ce3d565a3e3ffa4d5671c783d1afa64f
riagawali <101542435+riagawali@users.noreply.github.com>
2023-07-13 17:38:59 +0530

    User.controllers.js

--- controllers/User.controllers.js
+++ controllers/User.controllers.js
@@ -1,32 +1,3 @@
-// const User = require("../model/User");
-
-// const getUsers = async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.json(users);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to fetch users" });
-//   }
-// };
-
-// const createUser = async (req, res) => {
-//   try {
-//     const { email, name, password, phoneNumber } = req.body;
-//     const user = new User({ email, name, password, phoneNumber });
-//     await user.save();
-//     res.json({ message: "User created successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to create user" });
-//   }
-// };
-
-// module.exports = {
-//   getUsers,
-//   createUser,
-// };
-
 const userService = require("../services/userService");
 
 const createUser = async (req, res) => {
