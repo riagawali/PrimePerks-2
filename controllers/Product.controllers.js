ae0030ff01a358d00175f4df21ec3e18c79c4399
riagawali <101542435+riagawali@users.noreply.github.com>
2023-07-13 17:37:30 +0530

    Update Product.controllers.js

--- controllers/Product.controllers.js
+++ controllers/Product.controllers.js
@@ -1,32 +1,3 @@
-// const Product = require("../model/Product");
-
-// const getProducts = async (req, res) => {
-//   try {
-//     const products = await Product.find();
-//     res.json(products);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to fetch products" });
-//   }
-// };
-
-// const createProduct = async (req, res) => {
-//   try {
-//     const { name, description, pricePerMonth, paused } = req.body;
-//     const product = new Product({ name, description, pricePerMonth, paused });
-//     await product.save();
-//     res.json({ message: "Product created successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to create product" });
-//   }
-// };
-
-// module.exports = {
-//   getProducts,
-//   createProduct,
-// };
-
 const productService = require("../services/productService");
 
 const getProducts = async (req, res) => {
