ba935764d74e211d136b7baf25bd0101b79c69b1
riagawali <101542435+riagawali@users.noreply.github.com>
2023-07-13 17:38:19 +0530

    SubscriptionPlan.controllers.js

--- controllers/SubscriptionPlan.controllers.js
+++ controllers/SubscriptionPlan.controllers.js
@@ -1,37 +1,3 @@
-// const SubscriptionPlan = require("../model/SubscriptionPlan");
-
-// const getSubscriptionPlans = async (req, res) => {
-//   try {
-//     const subscriptionPlans = await SubscriptionPlan.find();
-//     res.json(subscriptionPlans);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to fetch subscription plans" });
-//   }
-// };
-
-// const createSubscriptionPlan = async (req, res) => {
-//   try {
-//     const { name, description, price, duration } = req.body;
-//     const subscriptionPlan = new SubscriptionPlan({
-//       name,
-//       description,
-//       price,
-//       duration,
-//     });
-//     await subscriptionPlan.save();
-//     res.json({ message: "Subscription plan created successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to create subscription plan" });
-//   }
-// };
-
-// module.exports = {
-//   getSubscriptionPlans,
-//   createSubscriptionPlan,
-// };
-
 const subscriptionPlanService = require("../services/subscriptionPlanService");
 
 const getSubscriptionPlans = async (req, res) => {
