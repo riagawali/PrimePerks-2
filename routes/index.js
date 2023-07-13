35eaf1fadd80ee576d8a55f992d69c2d08aa0a97
riagawali <101542435+riagawali@users.noreply.github.com>
2023-07-13 17:39:39 +0530

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const AdminController = require("../controllers/Admin.controllers");
-// const UserController = require("../controllers/User.controllers");
-// const ProductController = require("../controllers/Product.controllers");
-// const SubscriptionController = require("../controllers/Subscription.controllers");
-// const SubscriptionPlanController = require("../controllers/SubscriptionPlan.controllers");
-
-// router.post("/api/admins/register", AdminController.createAdmin);
-// // router.post("/api/admins/login", AdminController.login);
-
-// router.post("/api/users/register", UserController.createUser);
-// // router.post("/api/users/login", UserController.login);
-
-// router.get("/api/products", ProductController.getProducts);
-// router.post("/api/products", ProductController.createProduct);
-
-// // router.get("/api/subscriptions", SubscriptionController.getSubscriptions);
-// router.post("/api/subscriptions", SubscriptionController.createSubscription);
-
-// router.get(
-//   "/api/subscription-plans",
-//   SubscriptionPlanController.getSubscriptionPlans
-// );
-// router.post(
-//   "/api/subscription-plans",
-//   SubscriptionPlanController.createSubscriptionPlan
-// );
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
