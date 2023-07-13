37435acd54ab63a29a7ec600d03dce03ddb21ed2
riagawali <101542435+riagawali@users.noreply.github.com>
2023-07-13 17:37:07 +0530

    Update Admin.controllers.js

--- controllers/Admin.controllers.js
+++ controllers/Admin.controllers.js
@@ -1,21 +1,3 @@
-// const Admin = require("../model/Admin");
-
-// const createAdmin = async (req, res) => {
-//   try {
-//     const { email, name, password, phoneNumber } = req.body;
-//     const admin = new Admin({ email, name, password, phoneNumber });
-//     await admin.save();
-//     res.json({ message: "Admin created successfully" });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Failed to create admin" });
-//   }
-// };
-
-// module.exports = {
-//   createAdmin,
-// };
-
 const adminService = require("../services/adminService");
 
 const createAdmin = async (req, res) => {
